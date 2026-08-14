c2feea48d01e3d1ebf1413704e4ab7daf79dadc4
edgartikis <edgartikis@gmail.com>
2026-08-13 21:55:26 -0600

    Activa el conector de la nueva base de pruebas

--- align-test-db-config.js
+++ align-test-db-config.js
@@ -1,3 +1,3 @@
 // URL del despliegue de Google Apps Script para la base NUEVA de pruebas.
-// Déjala vacía hasta autorizar y desplegar Code.gs desde la hoja de pruebas.
-window.ALIGN_TEST_DB_URL = '';
+// Conector autorizado exclusivamente para la experiencia demo de GitHub Pages.
+window.ALIGN_TEST_DB_URL = 'https://script.google.com/macros/s/AKfycbxy2YY1Gk1afTFWQR26xs-0-Cz8widKXyk3h1a-_Bql-GhrWvwe7KIlAmKFDcKIlSEK/exec';
