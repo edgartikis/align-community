05e6acafa522e55a52f92d7edcefb7d24eb01c48
edgartikis <edgartikis@gmail.com>
2026-08-13 22:04:31 -0600

    Actualiza el despliegue de la base de pruebas

--- align-test-db-config.js
+++ align-test-db-config.js
@@ -1,3 +1,3 @@
 // URL del despliegue de Google Apps Script para la base NUEVA de pruebas.
 // Conector autorizado exclusivamente para la experiencia demo de GitHub Pages.
-window.ALIGN_TEST_DB_URL = 'https://script.google.com/macros/s/AKfycbxy2YY1Gk1afTFWQR26xs-0-Cz8widKXyk3h1a-_Bql-GhrWvwe7KIlAmKFDcKIlSEK/exec';
+window.ALIGN_TEST_DB_URL = 'https://script.google.com/macros/s/AKfycbzsORSI39oXxclg74SwVWStYBhsTOQGjlPZgmXS2hESl-y1T1XqjfbnYhsTcRK9qMV-/exec';
