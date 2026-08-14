51d1a8a43d67d9b2b105074b68481fb1bdda6f36
edgartikis <edgartikis@gmail.com>
2026-08-13 21:43:51 -0600

    Agrega configuración de base de pruebas

---
+++ align-test-db-config.js
@@ -0,0 +1,3 @@
+// URL del despliegue de Google Apps Script para la base NUEVA de pruebas.
+// Déjala vacía hasta autorizar y desplegar Code.gs desde la hoja de pruebas.
+window.ALIGN_TEST_DB_URL = '';
