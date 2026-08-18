3a09d643127408252b9b521c3ca0d71d96239998
edgartikis <edgartikis@gmail.com>
2026-08-17 22:09:12 -0600

    Restore ALIGN test database endpoint

--- align-test-db-config.js
+++ align-test-db-config.js
@@ -1,3 +1,3 @@
 // URL del despliegue de Google Apps Script para la base NUEVA de pruebas.
 // Conector autorizado exclusivamente para la experiencia demo de GitHub Pages.
-window.ALIGN_TEST_DB_URL = 'https://script.google.com/macros/s/AKfycbwlb66v_LE_EwmV-P8NBCIFx10G6Nfv_TbM9DOtx7KzB9NE1SWkh1nRcS6ozDJ3AAw2/exec';
+window.ALIGN_TEST_DB_URL = 'https://script.google.com/macros/s/AKfycbzsORSI39oXxclg74SwVWStYBhsTOQGjlPZgmXS2hESl-y1T1XqjfbnYhsTcRK9qMV-/exec';
